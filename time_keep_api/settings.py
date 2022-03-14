79c5788f14e877d0c89baa296a8fa617e6a2142c
Mohammed Ali Zubair <ali@Mohammeds-MacBook-Pro.local>
2022-03-14 01:31:13 -0400

    Changed static file storage mechanism

--- time_keep_api/settings.py
+++ time_keep_api/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.realpath(os.path.join(BASE_DIR, "static"))
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.realpath(os.path.join(BASE_DIR, "media"))
 
